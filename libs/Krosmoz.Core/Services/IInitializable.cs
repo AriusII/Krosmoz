88d86c3c382f8272e0cf7156eb401e3e1d99a653
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 03:27:09 +0200

    feat: add initialization contracts for services

---
+++ libs/Krosmoz.Core/Services/IInitializable.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Core.Services;
+
+/// <summary>
+/// Defines a contract for a service that requires initialization.
+/// </summary>
+public interface IInitializable
+{
+    /// <summary>
+    /// Performs the initialization logic for the service.
+    /// </summary>
+    void Initialize();
+}
---
+++ libs/Krosmoz.Core/Services/IOrderedInitializable.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Core.Services;
+
+/// <summary>
+/// Defines a contract for a service that requires initialization with a specific priority.
+/// </summary>
+public interface IOrderedInitializable : IInitializable
+{
+    /// <summary>
+    /// Gets the priority of the initialization process.
+    /// Higher priority values are initialized earlier.
+    /// </summary>
+    int Priority { get; }
+}
