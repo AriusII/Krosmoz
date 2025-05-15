66afb875976606e2178044ff2740f5ea9fc1957f
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 03:50:28 +0200

    feat: add GraphicalElementTypes enum to represent graphical elements types

---
+++ libs/Krosmoz.Serialization/ELE/GraphicalElementTypes.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.ELE;
+
+/// <summary>
+/// Represents the types of graphical elements.
+/// </summary>
+public enum GraphicalElementTypes
+{
+    /// <summary>
+    /// A normal graphical element.
+    /// </summary>
+    Normal = 0,
+
+    /// <summary>
+    /// A graphical element with a bounding box.
+    /// </summary>
+    BoundingBox = 1,
+
+    /// <summary>
+    /// An animated graphical element.
+    /// </summary>
+    Animated = 2,
+
+    /// <summary>
+    /// A graphical element representing an entity.
+    /// </summary>
+    Entity = 3,
+
+    /// <summary>
+    /// A graphical element with particle effects.
+    /// </summary>
+    Particles = 4,
+
+    /// <summary>
+    /// A blended graphical element.
+    /// </summary>
+    Blended = 5
+}
