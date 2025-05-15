26966b0c2003c129fea730a2f394c0c3963e15e1
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 14:58:35 +0200

    feat: add MetadataConstants for protocol build constants

---
+++ libs/Krosmoz.Serialization/Constants/MetadataConstants.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.Constants;
+
+public static class MetadataConstants
+{
+    public const int ProtocolBuild = 1547;
+    public const int ProtocolRequiredBuild = 1547;
+}
