810acddbb5a6f86b22817097a02a9fffd0623a46
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 14:45:53 +0200

    feat: add DatacenterConverter for symbol namespace and field updates

---
+++ tools/Krosmoz.Tools.Protocol/Converters/DatacenterConverter.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Extensions;
+using Krosmoz.Tools.Protocol.Models;
+
+namespace Krosmoz.Tools.Protocol.Converters;
+
+/// <summary>
+/// Provides functionality to convert a <see cref="DatacenterSymbol"/>.
+/// </summary>
+public sealed class DatacenterConverter : IConverter<DatacenterSymbol>
+{
+    /// <summary>
+    /// Converts the specified <see cref="DatacenterSymbol"/> by updating its namespace and field names
+    /// to follow the required conventions.
+    /// </summary>
+    /// <param name="symbol">The <see cref="DatacenterSymbol"/> to be converted.</param>
+    public void Convert(DatacenterSymbol symbol)
+    {
+        symbol.D2OClass.Namespace = symbol.D2OClass.Namespace.Contains("datacenter")
+            ? string.Concat("Krosmoz.Protocol", '.', string.Join('.', symbol.D2OClass.Namespace.Replace("com.ankamagames.dofus.", string.Empty).Split('.').Select(static x => x.Capitalize())))
+            : string.Concat("Krosmoz.Protocol.Datacenter", '.', string.Join('.', symbol.D2OClass.Namespace.Replace("com.ankamagames.dofus.", string.Empty).Split('.').Select(static x => x.Capitalize())));
+
+        symbol.D2OClass.Namespace = symbol.D2OClass.Namespace
+            .Replace("Com.Ankamagames.Tiphon.Types", "Tiphon")
+            .Replace("Flash.Geom", "Geom")
+            .Replace("Externalnotifications", "ExternalNotifications");
+
+        foreach (var d2OField in symbol.D2OClass.Fields)
+        {
+            if (d2OField.Name[0] is '_')
+                d2OField.Name = d2OField.Name[1..];
+
+            d2OField.Name = d2OField.Name.Capitalize();
+
+            if (d2OField.Name.Equals(symbol.D2OClass.Name))
+                d2OField.Name = string.Concat(d2OField.Name, '_');
+        }
+    }
+}
