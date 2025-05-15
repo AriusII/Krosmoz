b3e6a6a4b0c88c1c91d7999fa6663619a12e8452
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 14:58:21 +0200

    feat: add ProtocolConstants for serialization constraints

---
+++ libs/Krosmoz.Serialization/Constants/ProtocolConstants.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.Constants;
+
+public static class ProtocolConstants
+{
+    public const int MapCellCount = 560;
+    public const int MaxLevel = 200;
+    public const int MaxGuildLevel = 200;
+    public const int MaxMountLevel = 100;
+    public const int MaxChatLen = 512;
+    public const int UserMaxChatLen = 256;
+    public const int MinLoginLen = 3;
+    public const int MaxLoginLen = 50;
+    public const int MinPlayerNameLen = 2;
+    public const int MaxPlayerNameLen = 20;
+    public const int MinNickLen = 2;
+    public const int MaxNickLen = 30;
+    public const int MinGuildNameLen = 3;
+    public const int MaxGuildNameLen = 30;
+    public const int MinAllianceNameLen = 3;
+    public const int MaxAllianceNameLen = 30;
+    public const int MinAllianceTagLen = 3;
+    public const int MaxAllianceTagLen = 5;
+    public const int MaxPlayerColor = 5;
+    public const int MaxEntityColor = 8;
+    public const int MaxPlayersPerTeam = 8;
+    public const int MaxMembersPerParty = 8;
+    public const int MaxGuestsPerParty = 7;
+    public const int MaxMembersPerArenaParty = 5;
+    public const int MaxMonstersInGroupOnMap = 16;
+    public const int MaxChatObjectRef = 16;
+    public const int NotEquiped = 63;
+    public const int MaxEquiped = 255;
+    public const int MaxJobsPerCharacter = 6;
+    public const int MaxCraftSlot = 9;
+    public const int MaxJobLevel = 100;
+    public const int MaxHonor = 20000;
+    public const int MaxShortcut = 99;
+    public const uint MaxArenaRank = 2300;
+    public const int CharMinLevelArena = 50;
+    public const int CharMinLevelRide = 60;
+    public const int MaxDungeonRegister = 10;
+    public const int MinRideNameLen = 0;
+    public const int MaxRideNameLen = 16;
+    public const int MaxEffectsCount = 100;
+    public const int MaxLockableCodeLen = 8;
+    public const int MaxPresetItemCount = 24;
+    public const int MinObjCountByXfert = 1;
+    public const int MaxObjCountByXfert = 1000;
+    public const int MaxObjectEffectStringLen = 30;
+    public const int MaxPlayerOrAccountNameLen = 31;
+}
