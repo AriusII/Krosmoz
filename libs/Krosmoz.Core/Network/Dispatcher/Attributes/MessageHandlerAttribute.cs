5fda816668f8adeff59dbc8061b10cab0a100027
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 19:49:19 +0200

    feat: add MessageHandlerAttribute for marking network message handlers

---
+++ libs/Krosmoz.Core/Network/Dispatcher/Attributes/MessageHandlerAttribute.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Core.Network.Dispatcher.Attributes;
+
+/// <summary>
+/// Specifies that the attributed method is a message handler for network messages.
+/// </summary>
+/// <remarks>
+/// This attribute is used to mark methods that handle specific network messages
+/// in the dispatcher system.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class MessageHandlerAttribute : Attribute;
