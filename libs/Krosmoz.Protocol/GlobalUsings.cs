4ecfdba19faf266e95556f68930dd4447d9848ae
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 14:41:17 +0200

    feat: add global usings

---
+++ libs/Krosmoz.Protocol/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+global using Krosmoz.Core.IO.Binary;
+global using Krosmoz.Core.IO.Bits;
+global using Krosmoz.Core.Network.Metadata;
+global using Krosmoz.Serialization.D2O;
+global using Krosmoz.Serialization.D2O.Abstractions;
