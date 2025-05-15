8f37f405cade96ff44b4ce50e22891e424c9c341
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 03:47:54 +0200

    feat: add IDatacenterObject interface for D2O deserialization abstraction

---
+++ libs/Krosmoz.Serialization/D2O/Abstractions/IDatacenterObject.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.IO.Binary;
+
+namespace Krosmoz.Serialization.D2O.Abstractions;
+
+/// <summary>
+/// Represents a datacenter object that can be deserialized from a D2O file.
+/// </summary>
+public interface IDatacenterObject
+{
+    /// <summary>
+    /// Gets the module name associated with the datacenter object.
+    /// </summary>
+    static abstract string ModuleName { get; }
+
+    /// <summary>
+    /// Deserializes the datacenter object using the specified D2O class and binary reader.
+    /// </summary>
+    /// <param name="d2OClass">The D2O class containing metadata for deserialization.</param>
+    /// <param name="reader">The binary reader to read data from.</param>
+    void Deserialize(D2OClass d2OClass, BigEndianReader reader);
+}
