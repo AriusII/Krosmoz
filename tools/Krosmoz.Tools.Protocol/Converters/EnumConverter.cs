9603c50cf6a8bde33743e963037bf6c9ff945edb
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 14:45:10 +0200

    feat: add EnumConverter for transforming enum symbols and metadata

---
+++ tools/Krosmoz.Tools.Protocol/Converters/EnumConverter.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Humanizer;
+using Krosmoz.Tools.Protocol.Extensions;
+using Krosmoz.Tools.Protocol.Models;
+
+namespace Krosmoz.Tools.Protocol.Converters;
+
+/// <summary>
+/// Converts enumeration symbols by modifying their metadata and properties.
+/// </summary>
+public sealed class EnumConverter : IConverter<EnumSymbol>
+{
+    /// <summary>
+    /// Converts the provided enumeration symbol by updating its metadata name
+    /// and transforming its properties to adhere to specific naming and type conventions.
+    /// </summary>
+    /// <param name="symbol">The enumeration symbol to convert.</param>
+    public void Convert(EnumSymbol symbol)
+    {
+        if (symbol.Metadata.Name is "ActionIdConverter")
+            symbol.Metadata.Name = "ActionIds";
+
+        if (symbol.Metadata.Name.EndsWith("Enum"))
+            symbol.Metadata.Name = symbol.Metadata.Name
+                .Replace("Enum", string.Empty)
+                .Pluralize();
+        else
+            symbol.Metadata.Name = symbol.Metadata.Name.Pluralize();
+
+        foreach (var propertySymbol in symbol.Properties)
+            propertySymbol.Name = propertySymbol.Name.ToPascalCase();
+    }
+}
