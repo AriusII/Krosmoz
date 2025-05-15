87d065c0a1da6f7c8ec03f7bc0ce4f72819884cc
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 14:44:11 +0200

    feat: add IConverter interface for symbol conversion

---
+++ tools/Krosmoz.Tools.Protocol/Converters/IConverter.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Tools.Protocol.Converters;
+
+/// <summary>
+/// Defines a generic interface for converting symbols of a specific type.
+/// </summary>
+/// <typeparam name="TSymbol">The type of symbol to convert, constrained to reference types.</typeparam>
+public interface IConverter<in TSymbol>
+    where TSymbol : class
+{
+    /// <summary>
+    /// Converts the provided symbol into a desired format or representation.
+    /// </summary>
+    /// <param name="symbol">The symbol to convert.</param>
+    void Convert(TSymbol symbol);
+}
