4e7f02c0e9a6c3f2f0adb948aa3aa8f1e9dadb39
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 03:51:35 +0200

    feat: add GraphicalElementAdapter for loading graphical element files

---
+++ libs/Krosmoz.Serialization/ELE/GraphicalElementAdapter.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.IO.Binary;
+using Krosmoz.Core.IO.Compression;
+using Krosmoz.Serialization.Constants;
+
+namespace Krosmoz.Serialization.ELE;
+
+/// <summary>
+/// Provides methods for loading and adapting graphical element files.
+/// </summary>
+public static class GraphicalElementAdapter
+{
+    /// <summary>
+    /// Loads a graphical element file from the predefined path, decompressing it if necessary.
+    /// </summary>
+    /// <returns>A <see cref="GraphicalElementFile"/> instance containing the loaded data.</returns>
+    /// <exception cref="Exception">Thrown if the file header is invalid.</exception>
+    public static GraphicalElementFile Load()
+    {
+        var reader = new BigEndianReader(File.ReadAllBytes(PathConstants.Files.ElementsPath));
+
+        var header = reader.ReadInt8();
+
+        GraphicalElementFile file;
+
+        if (header is 69)
+        {
+            file = new GraphicalElementFile();
+            file.Load(reader);
+            return file;
+        }
+
+        reader.Seek(SeekOrigin.Begin, 0);
+
+        var deflatedBytes = ZipCompressor.Deflate(reader.ReadSpanToEnd().ToArray());
+
+        if (deflatedBytes.Length is 0 || deflatedBytes[0] is not 69)
+            throw new Exception("Invalid file header.");
+
+        reader = new BigEndianReader(deflatedBytes);
+        reader.Seek(SeekOrigin.Current, sizeof(sbyte));
+
+        file = new GraphicalElementFile();
+        file.Load(reader);
+        return file;
+    }
+}
