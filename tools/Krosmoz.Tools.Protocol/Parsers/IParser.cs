ff00fc1476d294821fcb3dd41b737c0e2d94f7d7
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 14:43:32 +0200

    feat: add IParser interface for parsing symbol metadata

---
+++ tools/Krosmoz.Tools.Protocol/Parsers/IParser.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Tools.Protocol.Models;
+
+namespace Krosmoz.Tools.Protocol.Parsers;
+
+/// <summary>
+/// Defines a generic interface for parsing symbol metadata into a specific type of symbol.
+/// </summary>
+/// <typeparam name="TSymbol">The type of symbol to parse, constrained to reference types.</typeparam>
+public interface IParser<out TSymbol>
+    where TSymbol : class
+{
+    /// <summary>
+    /// Parses the provided symbol metadata into an instance of <typeparamref name="TSymbol"/>.
+    /// </summary>
+    /// <param name="symbolMetadata">The metadata of the symbol to parse.</param>
+    /// <returns>An instance of <typeparamref name="TSymbol"/> representing the parsed symbol.</returns>
+    TSymbol Parse(SymbolMetadata symbolMetadata);
+}
