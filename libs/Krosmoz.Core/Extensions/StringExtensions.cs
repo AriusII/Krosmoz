6e89b752d8f710f4bf55341ce2b6e36aba89c75d
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-15 14:55:51 +0200

    feat: add methods to check and remove accents from strings

--- libs/Krosmoz.Core/Extensions/StringExtensions.cs
+++ libs/Krosmoz.Core/Extensions/StringExtensions.cs
@@ -3,6 +3,7 @@
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
 using System.Diagnostics.Contracts;
+using System.Globalization;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -13,6 +14,33 @@ namespace Krosmoz.Core.Extensions;
 /// </summary>
 public static class StringExtensions
 {
+    /// <summary>
+    /// Determines whether the specified string contains any accented characters.
+    /// </summary>
+    /// <param name="text">The string to check for accents.</param>
+    /// <returns><c>true</c> if the string contains accented characters; otherwise, <c>false</c>.</returns>
+    [Pure]
+    public static bool HasAccents(this string text)
+    {
+        return text
+            .Normalize(NormalizationForm.FormD)
+            .Any(static c => CharUnicodeInfo.GetUnicodeCategory(c) is UnicodeCategory.NonSpacingMark);
+    }
+
+    /// <summary>
+    /// Removes all accented characters from the specified string.
+    /// </summary>
+    /// <param name="text">The string from which to remove accents.</param>
+    /// <returns>A new string with all accented characters removed.</returns>
+    [Pure]
+    public static string RemoveAccents(this string text)
+    {
+        return string.Concat(text
+                .Normalize(NormalizationForm.FormD)
+                .Where(static c => CharUnicodeInfo.GetUnicodeCategory(c) is not UnicodeCategory.NonSpacingMark))
+            .Normalize(NormalizationForm.FormC);
+    }
+
     /// <summary>
     /// Capitalizes the first character of the specified string.
     /// </summary>
