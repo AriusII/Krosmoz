7ef02d8e05a46076447464df2c6191d63dadcc29
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 09:12:25 +0200

    feat: add DistinctBy extension for filtering distinct collection elements

---
+++ libs/Krosmoz.SourceGeneration/Infrastructure/Extensions/CollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.SourceGeneration.Infrastructure.Extensions;
+
+/// <summary>
+/// Provides extension methods for working with collections.
+/// </summary>
+public static class CollectionExtensions
+{
+    /// <summary>
+    /// Returns a sequence of distinct elements from the source collection based on a specified key selector.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the source collection.</typeparam>
+    /// <typeparam name="TKey">The type of the key used for determining distinct elements.</typeparam>
+    /// <param name="source">The source collection to filter for distinct elements.</param>
+    /// <param name="keySelector">A function to extract the key for each element.</param>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}"/> containing distinct elements from the source collection
+    /// based on the specified key.
+    /// </returns>
+    public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+    {
+        var seenKeys = new HashSet<TKey>();
+
+        foreach (var element in source)
+        {
+            if (seenKeys.Add(keySelector(element)))
+                yield return element;
+        }
+    }
+}
