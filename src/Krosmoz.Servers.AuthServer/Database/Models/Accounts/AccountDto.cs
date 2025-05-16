3ef70b3665b0bef2e639e9bac7e81773c7ee7e60
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 12:56:09 +0200

    feat: add AccountDto model

---
+++ src/Krosmoz.Servers.AuthServer/Database/Models/Accounts/AccountDto.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Serialization.I18N;
+
+namespace Krosmoz.Servers.AuthServer.Database.Models.Accounts;
+
+public sealed class AccountDto
+{
+    public required string Username { get; set; }
+
+    public required string Password { get; set; }
+
+    public required GameHierarchies Hierarchy { get; set; }
+
+    public required I18NLanguages Language { get; set; }
+
+    public required string SecretQuestion { get; set; }
+
+    public required string SecretAnswer { get; set; }
+}
