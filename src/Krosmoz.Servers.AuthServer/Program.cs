f16fd3f0ad0b89b2519d7fbc5f27b116a68bc51a
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 11:07:37 +0200

    feat: implement AuthServer with DI, logging, and message handling setup

--- src/Krosmoz.Servers.AuthServer/Program.cs
+++ src/Krosmoz.Servers.AuthServer/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Krosmoz.Core.Extensions;
+using Krosmoz.Core.Network.Dispatcher;
+using Krosmoz.Core.Network.Factory;
+using Krosmoz.Core.Network.Framing;
+using Krosmoz.Core.Network.Transport;
+using Krosmoz.Protocol.Datacenter;
+using Krosmoz.Protocol.Messages;
+using Krosmoz.Serialization.D2O.Abstractions;
+using Krosmoz.Serialization.Repository;
+using Krosmoz.Servers.AuthServer.Network.Transport;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+await Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(static logging => logging.UseSerilog())
+    .ConfigureServices(static (context, services) =>
+    {
+        services
+            .Configure<TcpServerOptions>(context.Configuration.GetSection("Server"))
+            .AddTransient<DofusMessageDecoder>()
+            .AddTransient<DofusMessageEncoder>()
+            .AddSingleton<IMessageFactory, MessageFactory>()
+            .AddSingleton<IDatacenterObjectFactory, DatacenterObjectFactory>()
+            .AddSingleton<IDatacenterRepository, DatacenterRepository>()
+            .AddMessageHandlers()
+            .AddHostedServiceAsSingleton<AuthServer>();
+    })
+    .RunConsoleAsync();
