2d7b9549276d42dcd0bb1c0ae23b91b1c3e972e7
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 21:40:03 +0200

    feat: add PingHandler to handle basic ping message responses

---
+++ src/Krosmoz.Servers.AuthServer/Handlers/Ping/PingHandler.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Network.Dispatcher.Attributes;
+using Krosmoz.Protocol.Messages.Common.Basic;
+using Krosmoz.Servers.AuthServer.Network.Transport;
+
+namespace Krosmoz.Servers.AuthServer.Handlers.Ping;
+
+/// <summary>
+/// Handles ping-related operations.
+/// </summary>
+public sealed class PingHandler
+{
+    /// <summary>
+    /// Handles the basic ping message asynchronously.
+    /// </summary>
+    /// <param name="session">The authentication session of the user.</param>
+    /// <param name="message">The basic ping message containing the ping details.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    [MessageHandler]
+    public async Task HandleBasicPingAsync(AuthSession session, BasicPingMessage message)
+    {
+        await session.SendAsync(new BasicPongMessage { Quiet = message.Quiet });
+    }
+}
