1935c9ff8ed7bb80c9d60da608f1d9cd06392fb1
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 11:57:34 +0200

    feat: add AccountRecord database model

---
+++ src/Krosmoz.Servers.AuthServer/Database/Models/Accounts/Account.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Net;
+using System.Net.NetworkInformation;
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Serialization.I18N;
+using Krosmoz.Servers.AuthServer.Database.Models.Servers;
+
+namespace Krosmoz.Servers.AuthServer.Database.Models.Accounts;
+
+public sealed class AccountRecord
+{
+    public int Id { get; init; }
+
+    public required string Username { get; set; }
+
+    public required string Password { get; set; }
+
+    public required GameHierarchies Hierarchy { get; set; }
+
+    public required I18NLanguages Language { get; set; }
+
+    public required string SecretQuestion { get; set; }
+
+    public required string SecretAnswer { get; set; }
+
+    public required DateTime CreatedAt { get; init; }
+
+    public required DateTime UpdatedAt { get; set; }
+
+    public required List<ServerCharacterRecord> Characters { get; init; }
+
+    public DateTime? SubscriptionExpireAt { get; set; }
+
+    public string? Nickname { get; set; }
+
+    public PhysicalAddress? MacAddress { get; set; }
+
+    public IPAddress? IpAddress { get; set; }
+
+    public string? Ticket { get; set; }
+
+    public bool HasRights =>
+        Hierarchy >= GameHierarchies.Moderator;
+}
