731fde8d61cb1a24c3cf0128955725817a506794
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 11:58:39 +0200

    feat: add serverscharacters migrations

---
+++ src/Krosmoz.Servers.AuthServer/Database/Migrations/20250516095643_ServersCharacters.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Krosmoz.Servers.AuthServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ServersCharacters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
