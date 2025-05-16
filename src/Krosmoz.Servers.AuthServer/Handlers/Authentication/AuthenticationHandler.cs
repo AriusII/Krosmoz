30054cb19a21edd5f604ef48fd00fb8a4649aa0d
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 11:07:23 +0200

    feat: add AuthenticationHandler for handling identification messages

---
+++ src/Krosmoz.Servers.AuthServer/Handlers/Authentication/AuthenticationHandler.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Network.Dispatcher.Attributes;
+using Krosmoz.Protocol.Messages.Connection;
+using Krosmoz.Servers.AuthServer.Network.Transport;
+
+namespace Krosmoz.Servers.AuthServer.Handlers.Authentication;
+
+public sealed class AuthenticationHandler
+{
+    [MessageHandler]
+    public Task HandleIdentificationAsync(AuthSession session, IdentificationMessage message)
+    {
+        return Task.CompletedTask;
+    }
+}
