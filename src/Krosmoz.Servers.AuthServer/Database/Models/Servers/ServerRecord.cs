3fe27d2f570644733e7d64d30ba3fa5ca91f1eed
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 12:02:01 +0200

    feat: add ServerRecord database model

---
+++ src/Krosmoz.Servers.AuthServer/Database/Models/Servers/ServerRecord.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Net;
+using Krosmoz.Protocol.Enums;
+using Krosmoz.Protocol.Enums.Custom;
+
+namespace Krosmoz.Servers.AuthServer.Database.Models.Servers;
+
+public sealed class ServerRecord
+{
+    public required int Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required ServerGameTypeIds Type { get; init; }
+
+    public required ServerCommunityIds Community { get; init; }
+
+    public required ServerStatuses Status { get; set; }
+
+    public required GameHierarchies VisibleHierarchy { get; set; }
+
+    public required GameHierarchies JoinableHierarchy { get; set; }
+
+    public IPAddress? IpAddress { get; set; }
+
+    public int? Port { get; set; }
+
+    public DateTime? OpenedAt { get; set; }
+}
