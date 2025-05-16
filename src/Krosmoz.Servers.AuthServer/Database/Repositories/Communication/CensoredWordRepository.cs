4313a393537b49b73d87add047e406716c98f804
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 21:43:02 +0200

    feat: add repository to manage censored words in the database

---
+++ src/Krosmoz.Servers.AuthServer/Database/Repositories/Communication/CensoredWordRepository.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Collections.Frozen;
+using Krosmoz.Core.Services;
+using Krosmoz.Protocol.Datacenter.Communication;
+using Krosmoz.Serialization.I18N;
+using Krosmoz.Serialization.Repository;
+
+namespace Krosmoz.Servers.AuthServer.Database.Repositories.Communication;
+
+/// <summary>
+/// Represents a repository for managing censored words in the database.
+/// </summary>
+public sealed class CensoredWordRepository : InitializableService, ICensoredWordRepository
+{
+    private readonly IDatacenterRepository _datacenterRepository;
+
+    private FrozenSet<CensoredWord> _censoredWords;
+
+    /// <summary>
+    /// Initializes the repository by loading censored words from the datacenter repository
+    /// and storing them in a frozen set for efficient access.
+    /// </summary>
+    protected override void Initialize()
+    {
+        _censoredWords = _datacenterRepository.GetObjects<CensoredWord>().ToFrozenSet();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CensoredWordRepository"/> class.
+    /// </summary>
+    /// <param name="datacenterRepository">The repository for accessing datacenter objects.</param>
+    public CensoredWordRepository(IDatacenterRepository datacenterRepository)
+    {
+        _datacenterRepository = datacenterRepository;
+        _censoredWords = [];
+    }
+
+    /// <summary>
+    /// Retrieves a collection of censored words for the specified language.
+    /// </summary>
+    /// <param name="language">The language for which to retrieve censored words.</param>
+    /// <returns>An enumerable collection of censored words for the specified language.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the specified language is not supported.</exception>
+    public IEnumerable<CensoredWord> GetCensoredWords(I18NLanguages language)
+    {
+        var shortName = language switch
+        {
+            I18NLanguages.English => "en",
+            I18NLanguages.French => "fr",
+            I18NLanguages.Spanish => "es",
+            _ => throw new ArgumentOutOfRangeException(nameof(language))
+        };
+
+        return _censoredWords.Where(x => x.Language.Equals(shortName));
+    }
+}
---
+++ src/Krosmoz.Servers.AuthServer/Database/Repositories/Communication/ICensoredWordRepository.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Datacenter.Communication;
+using Krosmoz.Serialization.I18N;
+
+namespace Krosmoz.Servers.AuthServer.Database.Repositories.Communication;
+
+/// <summary>
+/// Defines the contract for a repository that manages censored words in the database.
+/// </summary>
+public interface ICensoredWordRepository
+{
+    /// <summary>
+    /// Retrieves a collection of censored words for the specified language.
+    /// </summary>
+    /// <param name="language">The language for which to retrieve censored words.</param>
+    /// <returns>An enumerable collection of censored words.</returns>
+    IEnumerable<CensoredWord> GetCensoredWords(I18NLanguages language);
+}
