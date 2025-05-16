ec9ab42538b9c3d6bbf7e81d6a7ebd5101741ac6
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-16 11:57:23 +0200

    feat: add ServerCharacterRecord database model

---
+++ src/Krosmoz.Servers.AuthServer/Database/Models/Servers/ServerCharacterRecord.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.AuthServer.Database.Models.Servers;
+
+public sealed class ServerCharacterRecord
+{
+    public int Id { get; init; }
+
+    public required int ServerId { get; init; }
+
+    public required int AccountId { get; init; }
+
+    public required long CharacterId { get; init; }
+}
