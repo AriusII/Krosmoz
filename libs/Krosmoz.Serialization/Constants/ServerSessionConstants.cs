e3e95793744e53ce9c93f49b61d2e164c386d37a
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 01:57:22 +0200

    feat: add ServerSessionConstants for session-related configuration

---
+++ libs/Krosmoz.Serialization/Constants/ServerSessionConstants.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Serialization.Constants;
+
+public static class ServerSessionConstants
+{
+    public const int TimeBeforeDisconnection = 1;
+
+    public const int KohDuration = 2;
+
+    public const int KohWinningScore = 3;
+
+    public const int MinimalTimeBeforeKoh = 4;
+
+    public const int MinimalTimeWeighInKoh = 5;
+}
