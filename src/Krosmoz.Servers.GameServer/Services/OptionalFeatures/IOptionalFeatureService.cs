214a33b6bf8bc5d868dfa4e456bd0b2715629b19
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 01:50:43 +0200

    feat: add optional feature service with feature management methods

---
+++ src/Krosmoz.Servers.GameServer/Services/OptionalFeatures/IOptionalFeatureService.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums.Custom;
+
+namespace Krosmoz.Servers.GameServer.Services.OptionalFeatures;
+
+/// <summary>
+/// Defines the contract for a service that manages optional features in the game server.
+/// </summary>
+public interface IOptionalFeatureService
+{
+    /// <summary>
+    /// Retrieves a collection of enabled optional features.
+    /// </summary>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}"/> containing the IDs of the enabled optional features.
+    /// </returns>
+    IEnumerable<OptionalFeatureIds> GetEnabledFeatures();
+
+    /// <summary>
+    /// Determines whether a specific optional feature is enabled.
+    /// </summary>
+    /// <param name="featureId">The ID of the optional feature to check.</param>
+    /// <returns>
+    /// <c>true</c> if the feature is enabled; otherwise, <c>false</c>.
+    /// </returns>
+    bool IsFeatureEnabled(OptionalFeatureIds featureId);
+}
---
+++ src/Krosmoz.Servers.GameServer/Services/OptionalFeatures/OptionalFeatureService.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Protocol.Enums.Custom;
+using Microsoft.Extensions.Configuration;
+
+namespace Krosmoz.Servers.GameServer.Services.OptionalFeatures;
+
+/// <summary>
+/// Provides functionality to manage optional features in the game server.
+/// </summary>
+public sealed class OptionalFeatureService : IOptionalFeatureService
+{
+    private readonly IConfiguration _configuration;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OptionalFeatureService"/> class.
+    /// </summary>
+    /// <param name="configuration">The configuration instance used to retrieve enabled optional features.</param>
+    public OptionalFeatureService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    /// <summary>
+    /// Retrieves a collection of enabled optional features.
+    /// </summary>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}"/> containing the IDs of the enabled optional features.
+    /// </returns>
+    public IEnumerable<OptionalFeatureIds> GetEnabledFeatures()
+    {
+        return _configuration.GetValue<OptionalFeatureIds[]>("OptionalFeatures")!;
+    }
+
+    /// <summary>
+    /// Determines whether a specific optional feature is enabled.
+    /// </summary>
+    /// <param name="featureId">The ID of the optional feature to check.</param>
+    /// <returns>
+    /// <c>true</c> if the feature is enabled; otherwise, <c>false</c>.
+    /// </returns>
+    public bool IsFeatureEnabled(OptionalFeatureIds featureId)
+    {
+        return _configuration.GetValue<OptionalFeatureIds[]>("OptionalFeatures")!.Contains(featureId);
+    }
+}
