b6f397481ce830a57e25b7d8f189337537df4e45
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-17 22:04:05 +0200

    feat: make ElementId non-nullable in interactives table

---
+++ src/Krosmoz.Servers.GameServer/Database/Migrations/20250517200302_InteractivesSwitchNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Krosmoz.Servers.GameServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InteractivesSwitchNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ElementId",
+                table: "interactives",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ElementId",
+                table: "interactives",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
