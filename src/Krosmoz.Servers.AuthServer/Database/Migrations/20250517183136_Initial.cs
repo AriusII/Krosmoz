ef442a6ed573062dd478ac6d34953c32852b4953
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-17 20:35:13 +0200

    feat: add initial database migration for AuthServer

---
+++ src/Krosmoz.Servers.AuthServer/Database/Migrations/20250517183136_Initial.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Krosmoz.Servers.AuthServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Hierarchy = table.Column<int>(type: "integer", nullable: false),
+                    Language = table.Column<int>(type: "integer", nullable: false),
+                    SecretQuestion = table.Column<string>(type: "text", nullable: false),
+                    SecretAnswer = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SubscriptionExpireAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Nickname = table.Column<string>(type: "text", nullable: true),
+                    MacAddress = table.Column<string>(type: "text", nullable: true),
+                    IpAddress = table.Column<IPAddress>(type: "inet", nullable: true),
+                    Ticket = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "servers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Community = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    VisibleHierarchy = table.Column<int>(type: "integer", nullable: false),
+                    JoinableHierarchy = table.Column<int>(type: "integer", nullable: false),
+                    IpAddress = table.Column<IPAddress>(type: "inet", nullable: true),
+                    Port = table.Column<int>(type: "integer", nullable: true),
+                    OpenedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_servers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "servers_characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ServerId = table.Column<int>(type: "integer", nullable: false),
+                    AccountId = table.Column<int>(type: "integer", nullable: false),
+                    CharacterId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_servers_characters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_servers_characters_accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "accounts",
+                columns: new[] { "Id", "CreatedAt", "Hierarchy", "IpAddress", "Language", "MacAddress", "Nickname", "Password", "SecretAnswer", "SecretQuestion", "SubscriptionExpireAt", "Ticket", "UpdatedAt", "Username" },
+                values: new object[] { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 40, null, 0, null, null, "21232f297a57a5a743894a0e4a801fc3", "blue", "What is your favorite color?", null, null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "admin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_accounts_Nickname",
+                table: "accounts",
+                column: "Nickname",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_accounts_Ticket",
+                table: "accounts",
+                column: "Ticket",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_accounts_Username",
+                table: "accounts",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_servers_characters_AccountId",
+                table: "servers_characters",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_servers_characters_ServerId",
+                table: "servers_characters",
+                column: "ServerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "servers");
+
+            migrationBuilder.DropTable(
+                name: "servers_characters");
+
+            migrationBuilder.DropTable(
+                name: "accounts");
+        }
+    }
+}
