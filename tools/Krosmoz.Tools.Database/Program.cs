4ba41c8f62d11b6d2db7bc54fbe6c04765352c1b
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-17 19:04:26 +0200

    feat: add GameDbContext support in Program.cs

--- tools/Krosmoz.Tools.Database/Program.cs
+++ tools/Krosmoz.Tools.Database/Program.cs
@@ -3,6 +3,7 @@ using Krosmoz.Protocol.Datacenter;
 using Krosmoz.Serialization.D2O.Abstractions;
 using Krosmoz.Serialization.Repository;
 using Krosmoz.Servers.AuthServer.Database;
+using Krosmoz.Servers.GameServer.Database;
 using Krosmoz.Tools.Database.Base;
 using Krosmoz.Tools.Database.Hosting;
 using Krosmoz.Tools.Database.Servers;
@@ -16,6 +17,7 @@ await Host.CreateDefaultBuilder(args)
     {
         services
             .AddDbContext<AuthDbContext>(context.Configuration.GetConnectionString("Auth"))
+            .AddDbContext<GameDbContext>(context.Configuration.GetConnectionString("Game"))
             .AddSingleton<IDatacenterObjectFactory, DatacenterObjectFactory>()
             .AddSingleton<IDatacenterRepository, DatacenterRepository>()
             .AddSingleton<BaseSynchronizer, ServerSynchronizer>()
