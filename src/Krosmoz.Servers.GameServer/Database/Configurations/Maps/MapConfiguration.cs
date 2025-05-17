d15a357ebed7b120704e27c3d9e477653c153c44
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-17 18:49:00 +0200

    feat: add Map entity models and EFCore configuration

---
+++ src/Krosmoz.Servers.GameServer/Database/Configurations/Maps/MapConfiguration.cs
@@ -0,0 +1,144 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Servers.GameServer.Database.Models.Maps;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Krosmoz.Servers.GameServer.Database.Configurations.Maps;
+
+/// <summary>
+/// Configures the database schema for the <see cref="MapRecord"/> entity.
+/// </summary>
+public sealed class MapConfiguration : IEntityTypeConfiguration<MapRecord>
+{
+    /// <summary>
+    /// Configures the properties and relationships of the <see cref="MapRecord"/> entity.
+    /// </summary>
+    /// <param name="builder">The builder used to configure the entity.</param>
+    public void Configure(EntityTypeBuilder<MapRecord> builder)
+    {
+        builder.HasKey(static x => x.Id);
+
+        builder
+            .Property(static x => x.Id)
+            .ValueGeneratedNever();
+
+        builder
+            .Property(static x => x.X)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.Y)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.Outdoor)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.Capabilities)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.SubAreaId)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.WorldMap)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.HasPriorityOnWorldMap)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.PrismAllowed)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.PvpDisabled)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.PlacementGenDisabled)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.MerchantsMax)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.SpawnDisabled)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.RedCells)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.BlueCells)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.Cells)
+            .HasConversion(
+                static x => SerializeCells(x),
+                static x => DeserializeCells(x),
+                new ValueComparer<long[]>(
+                    static (x, y) => ReferenceEquals(x, y) || x != null && y != null && x.SequenceEqual(y),
+                    static x => x.Aggregate(0, static (a, b) => HashCode.Combine(a, b)),
+                    static x => x.ToArray()))
+            .IsRequired();
+
+        builder
+            .Property(static x => x.TopNeighborId)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.BottomNeighborId)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.LeftNeighborId)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.RightNeighborId)
+            .IsRequired();
+
+        builder.ToTable("maps");
+    }
+
+    /// <summary>
+    /// Serializes an array of <see cref="CellData"/> into an array of long values.
+    /// </summary>
+    /// <param name="cells">The array of <see cref="CellData"/> to serialize.</param>
+    /// <returns>An array of long values representing the serialized cells.</returns>
+    private static long[] SerializeCells(CellData[] cells)
+    {
+        var newCells = new long[cells.Length];
+
+        for (var i = 0; i < cells.Length; i++)
+            newCells[i] = cells[i].Data;
+
+        return newCells;
+    }
+
+    /// <summary>
+    /// Deserializes an array of long values into an array of <see cref="CellData"/>.
+    /// </summary>
+    /// <param name="cells">The array of long values to deserialize.</param>
+    /// <returns>An array of <see cref="CellData"/> representing the deserialized cells.</returns>
+    private static CellData[] DeserializeCells(long[] cells)
+    {
+        var newCells = new CellData[cells.Length];
+
+        for (var i = 0; i < cells.Length; i++)
+            newCells[i] = new CellData { Data = cells[i] };
+
+        return newCells;
+    }
+}
---
+++ src/Krosmoz.Servers.GameServer/Database/Models/Maps/MapRecord.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.GameServer.Database.Models.Maps;
+
+public sealed class MapRecord
+{
+    public required int Id { get; init; }
+
+    public required int X { get; init; }
+
+    public required int Y { get; init; }
+
+    public required bool Outdoor { get; init; }
+
+    public required int Capabilities { get; init; }
+
+    public required int SubAreaId { get; init; }
+
+    public required int WorldMap { get; init; }
+
+    public required bool HasPriorityOnWorldMap { get; init; }
+
+    public required bool PrismAllowed { get; init; }
+
+    public required bool PvpDisabled { get; init; }
+
+    public required bool PlacementGenDisabled { get; init; }
+
+    public required int MerchantsMax { get; init; }
+
+    public required int SpawnDisabled { get; init; }
+
+    public required short[] RedCells { get; init; }
+
+    public required short[] BlueCells { get; init; }
+
+    public required CellData[] Cells { get; init; }
+
+    public required int TopNeighborId { get; init; }
+
+    public required int BottomNeighborId { get; init; }
+
+    public required int LeftNeighborId { get; init; }
+
+    public required int RightNeighborId { get; init; }
+
+    public short? TopCellId { get; init; }
+
+    public short? BottomCellId { get; init; }
+
+    public short? LeftCellId { get; init; }
+
+    public short? RightCellId { get; init; }
+}
