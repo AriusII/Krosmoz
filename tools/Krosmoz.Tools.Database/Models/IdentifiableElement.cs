dc619ed2f5ea5520fce60706aa930eb4940f535a
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-17 22:04:14 +0200

    feat: add IdentifiableElement model to represent graphical elements

---
+++ tools/Krosmoz.Tools.Database/Models/IdentifiableElement.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Serialization.DLM;
+using Krosmoz.Serialization.DLM.Elements;
+
+namespace Krosmoz.Tools.Database.Models;
+
+public sealed class IdentifiableElement
+{
+    public required DlmGraphicalElement Element { get; init; }
+
+    public required DlmMap Map { get; init; }
+
+    public int GfxId { get; set; }
+
+    public bool Animated { get; set; }
+}
