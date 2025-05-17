ab0b9a90bc34bdc003ff1a214380eaef9f6adb09
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 01:43:24 +0200

    feat: add GameServer and GameSession for handling game server communication

---
+++ src/Krosmoz.Servers.GameServer/Network/Transport/GameServer.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Core.Network.Transport;
+using Krosmoz.Protocol.Messages.Game.Approach;
+using Krosmoz.Protocol.Messages.Handshake;
+using Krosmoz.Serialization.Constants;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Krosmoz.Servers.GameServer.Network.Transport;
+
+/// <summary>
+/// Represents the GameServer class, which is responsible for managing the network transport
+/// and communication for the game server using TCP connections.
+/// </summary>
+public sealed class GameServer : TcpServer<GameSession>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameServer"/> class.
+    /// </summary>
+    /// <param name="services">The service provider for resolving dependencies.</param>
+    /// <param name="logger">The logger used for logging server-related events.</param>
+    /// <param name="options">The options for configuring the TCP server.</param>
+    public GameServer(IServiceProvider services, ILogger<TcpServer<GameSession>> logger, IOptions<TcpServerOptions> options)
+        : base(services, logger, options)
+    {
+    }
+
+    /// <summary>
+    /// Handles the event when a new session is connected to the server.
+    /// Sends the required protocol version and a hello message to the client.
+    /// </summary>
+    /// <param name="session">The session that has connected.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    protected override async Task OnSessionConnectedAsync(GameSession session)
+    {
+        await session.SendAsync(new ProtocolRequired { RequiredVersion = MetadataConstants.ProtocolRequiredBuild, CurrentVersion = MetadataConstants.ProtocolBuild });
+        await session.SendAsync<HelloGameMessage>();
+    }
+}
---
+++ src/Krosmoz.Servers.GameServer/Network/Transport/GameSession.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Net.Sockets;
+using Krosmoz.Core.Network.Dispatcher;
+using Krosmoz.Core.Network.Factory;
+using Krosmoz.Core.Network.Framing;
+using Krosmoz.Core.Network.Transport;
+using Microsoft.Extensions.Logging;
+
+namespace Krosmoz.Servers.GameServer.Network.Transport;
+
+/// <summary>
+/// Represents a game session that manages communication and message handling
+/// for a connected client in the game server.
+/// </summary>
+public sealed class GameSession : TcpSession
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameSession"/> class.
+    /// </summary>
+    /// <param name="socket">The socket used for the session's communication.</param>
+    /// <param name="messageDecoder">The decoder used to decode incoming messages.</param>
+    /// <param name="messageEncoder">The encoder used to encode outgoing messages.</param>
+    /// <param name="messageDispatcher">The dispatcher responsible for handling messages.</param>
+    /// <param name="messageFactory">The factory used to create message instances.</param>
+    /// <param name="logger">The logger used for logging session-related events.</param>
+    public GameSession(
+        Socket socket,
+        DofusMessageDecoder messageDecoder,
+        DofusMessageEncoder messageEncoder,
+        IMessageDispatcher messageDispatcher,
+        IMessageFactory messageFactory,
+        ILogger<TcpSession> logger)
+        : base(socket, messageDecoder, messageEncoder, messageDispatcher, messageFactory, logger)
+    {
+    }
+}
