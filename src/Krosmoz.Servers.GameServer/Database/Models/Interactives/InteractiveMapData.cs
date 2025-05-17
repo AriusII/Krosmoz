d2f8948759b77eca058c209435c63702e87bad44
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-17 21:14:48 +0200

    feat: add InteractiveMapData and InteractiveRecord models

---
+++ src/Krosmoz.Servers.GameServer/Database/Models/Interactives/InteractiveMapData.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using System.Runtime.InteropServices;
+
+namespace Krosmoz.Servers.GameServer.Database.Models.Interactives;
+
+[StructLayout(LayoutKind.Explicit)]
+public struct InteractiveMapData
+{
+    [FieldOffset(0)]
+    public int MapId;
+
+    [FieldOffset(4)]
+    public bool OnMap;
+
+    [FieldOffset(5)]
+    public short CellId;
+
+    [FieldOffset(0)]
+    public long Data;
+}
---
+++ src/Krosmoz.Servers.GameServer/Database/Models/Interactives/InteractiveRecord.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.GameServer.Database.Models.Interactives;
+
+public sealed class InteractiveRecord
+{
+    public required int Id { get; init; }
+
+    public required int GfxId { get; init; }
+
+    public required bool Animated { get; init; }
+
+    public required int MapId { get; init; }
+
+    public int? ElementId { get; init; }
+
+    public required InteractiveMapData[] MapsData { get; init; }
+}
