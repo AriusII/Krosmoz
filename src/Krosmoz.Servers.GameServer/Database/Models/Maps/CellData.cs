6e2bea9256c6939e5530a5addd119c36de02d18b
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-17 22:38:08 +0200

    refactor: remove unnecessary StructLayout and FieldOffset attributes

--- src/Krosmoz.Servers.GameServer/Database/Models/Maps/CellData.cs
+++ src/Krosmoz.Servers.GameServer/Database/Models/Maps/CellData.cs
@@ -2,31 +2,19 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
-using System.Runtime.InteropServices;
-
 namespace Krosmoz.Servers.GameServer.Database.Models.Maps;
 
-[StructLayout(LayoutKind.Explicit)]
 public struct CellData
 {
-    [FieldOffset(0)]
     public short Id;
 
-    [FieldOffset(2)]
     public byte MapChangeData;
 
-    [FieldOffset(3)]
     public byte MoveZone;
 
-    [FieldOffset(4)]
     public sbyte Speed;
 
-    [FieldOffset(5)]
     public byte Floor;
 
-    [FieldOffset(6)]
     public byte LosMov;
-
-    [FieldOffset(0)]
-    public long Data;
 }
