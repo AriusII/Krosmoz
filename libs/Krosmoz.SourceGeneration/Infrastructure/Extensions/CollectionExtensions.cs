3d11cb4fec0faaf06b22fb70a6c7a2a2127c5536
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-17 09:04:52 +0200

    feat: add Index extension method to enumerate collection with indices

--- libs/Krosmoz.SourceGeneration/Infrastructure/Extensions/CollectionExtensions.cs
+++ libs/Krosmoz.SourceGeneration/Infrastructure/Extensions/CollectionExtensions.cs
@@ -30,4 +30,24 @@ public static class CollectionExtensions
                 yield return element;
         }
     }
+
+    /// <summary>
+    /// Enumerates a collection and returns each element along with its index.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="collection">The collection to enumerate.</param>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}"/> of tuples where each tuple contains the index
+    /// and the corresponding element from the collection.
+    /// </returns>
+    public static IEnumerable<(int, T)> Index<T>(this IEnumerable<T> collection)
+    {
+        var index = -1;
+
+        foreach (var element in collection)
+        {
+            index++;
+            yield return (index, element);
+        }
+    }
 }
