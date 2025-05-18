32141ec72eb907c2bf6027a93ca279354fc3cfd2
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 18:35:38 +0200

    feat: add DbSet for ExperienceRecord in GameDbContext

--- src/Krosmoz.Servers.GameServer/Database/GameDbContext.cs
+++ src/Krosmoz.Servers.GameServer/Database/GameDbContext.cs
@@ -2,6 +2,7 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
+using Krosmoz.Servers.GameServer.Database.Models.Experiences;
 using Krosmoz.Servers.GameServer.Database.Models.Interactives;
 using Krosmoz.Servers.GameServer.Database.Models.Maps;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,11 @@ public sealed class GameDbContext : DbContext
     /// Gets or sets the DbSet representing the collection of <see cref="InteractiveRecord"/> entities.
     /// </summary>
     public required DbSet<InteractiveRecord> Interactives { get; set; }
+
+    /// <summary>
+    /// Gets or sets the DbSet representing the collection of <see cref="ExperienceRecord"/> entities.
+    /// </summary>
+    public required DbSet<ExperienceRecord> Experiences { get; set; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="GameDbContext"/> class with the specified options.
