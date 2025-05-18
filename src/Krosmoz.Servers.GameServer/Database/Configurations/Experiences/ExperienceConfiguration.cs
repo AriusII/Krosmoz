2f4ce07062afe03506239e329d65ac70e039a4d6
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 18:34:18 +0200

    feat: add ExperienceConfiguration for database schema setup

---
+++ src/Krosmoz.Servers.GameServer/Database/Configurations/Experiences/ExperienceConfiguration.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+using Krosmoz.Servers.GameServer.Database.Models.Experiences;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Krosmoz.Servers.GameServer.Database.Configurations.Experiences;
+
+/// <summary>
+/// Configures the database schema for the <see cref="ExperienceRecord"/> entity.
+/// </summary>
+public sealed class ExperienceConfiguration : IEntityTypeConfiguration<ExperienceRecord>
+{
+    /// <summary>
+    /// Configures the properties and relationships of the <see cref="ExperienceRecord"/> entity.
+    /// </summary>
+    /// <param name="builder">The builder used to configure the entity.</param>
+    public void Configure(EntityTypeBuilder<ExperienceRecord> builder)
+    {
+        builder.HasKey(static x => x.Level);
+
+        builder
+            .Property(static x => x.Level)
+            .ValueGeneratedNever();
+
+        builder
+            .Property(static x => x.CharacterXp)
+            .IsRequired();
+
+        builder
+            .Property(static x => x.GuildXp)
+            .IsRequired();
+
+        builder.ToTable("experiences");
+    }
+}
