c33f51f8124153f389f76f478feb2c04cca61fb7
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 21:12:43 +0200

    feat: add methods to retrieve breed and head data to repository

--- src/Krosmoz.Servers.GameServer/Database/Repositories/Breeds/BreedRepository.cs
+++ src/Krosmoz.Servers.GameServer/Database/Repositories/Breeds/BreedRepository.cs
@@ -3,6 +3,7 @@
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
 using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
 using Krosmoz.Core.Extensions;
 using Krosmoz.Core.Services;
 using Krosmoz.Protocol.Datacenter.Breeds;
@@ -19,8 +20,8 @@ public sealed class BreedRepository : IBreedRepository, IInitializableService
 {
     private readonly IDatacenterRepository _datacenterRepository;
 
-    // Stores breed data indexed by breed identifiers.
     private ConcurrentDictionary<BreedIds, Breed> _breeds;
+    private ConcurrentDictionary<int, Head> _heads;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="BreedRepository"/> class.
@@ -30,6 +31,7 @@ public sealed class BreedRepository : IBreedRepository, IInitializableService
     {
         _datacenterRepository = datacenterRepository;
         _breeds = [];
+        _heads = [];
     }
 
     /// <summary>
@@ -38,15 +40,38 @@ public sealed class BreedRepository : IBreedRepository, IInitializableService
     public void Initialize()
     {
         _breeds = _datacenterRepository.GetObjects<Breed>().ToConcurrentDictionary(static x => (BreedIds)x.Id);
+        _heads = _datacenterRepository.GetObjects<Head>().ToConcurrentDictionary(static x => x.Id);
     }
 
     /// <summary>
-    /// Retrieves the starting spell identifiers for a specific breed.
+    /// Attempts to retrieve breed data for the specified breed ID.
     /// </summary>
-    /// <param name="breedId">The identifier of the breed.</param>
-    /// <returns>An enumerable collection of <see cref="SpellIds"/> representing the starting spells for the specified breed.</returns>
-    public IEnumerable<SpellIds> GetStartSpellIds(BreedIds breedId)
+    /// <param name="breedId">The ID of the breed to retrieve.</param>
+    /// <param name="breed">
+    /// When this method returns, contains the breed data if the breed ID exists; otherwise, null.
+    /// This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>
+    /// True if the breed data was successfully retrieved; otherwise, false.
+    /// </returns>
+    public bool TryGetBreed(BreedIds breedId, [NotNullWhen(true)] out Breed? breed)
     {
-        return _breeds[breedId].BreedSpellsId.Select(static x => (SpellIds)x);
+        return _breeds.TryGetValue(breedId, out breed);
+    }
+
+    /// <summary>
+    /// Attempts to retrieve head data for the specified cosmetic ID.
+    /// </summary>
+    /// <param name="cosmeticId">The ID of the cosmetic to retrieve the head data for.</param>
+    /// <param name="head">
+    /// When this method returns, contains the head data if the cosmetic ID exists; otherwise, null.
+    /// This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>
+    /// True if the head data was successfully retrieved; otherwise, false.
+    /// </returns>
+    public bool TryGetHead(int cosmeticId, [NotNullWhen(true)] out Head? head)
+    {
+        return _heads.TryGetValue(cosmeticId, out head);
     }
 }
--- src/Krosmoz.Servers.GameServer/Database/Repositories/Breeds/IBreedRepository.cs
+++ src/Krosmoz.Servers.GameServer/Database/Repositories/Breeds/IBreedRepository.cs
@@ -2,6 +2,8 @@
 // Krosmoz licenses this file to you under the MIT license.
 // See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
 
+using System.Diagnostics.CodeAnalysis;
+using Krosmoz.Protocol.Datacenter.Breeds;
 using Krosmoz.Protocol.Enums.Custom;
 
 namespace Krosmoz.Servers.GameServer.Database.Repositories.Breeds;
@@ -12,9 +14,28 @@ namespace Krosmoz.Servers.GameServer.Database.Repositories.Breeds;
 public interface IBreedRepository
 {
     /// <summary>
-    /// Retrieves the starting spell identifiers for a specific breed.
+    /// Attempts to retrieve breed data for the specified breed ID.
     /// </summary>
-    /// <param name="breedId">The identifier of the breed.</param>
-    /// <returns>An enumerable collection of <see cref="SpellIds"/> representing the starting spells for the specified breed.</returns>
-    IEnumerable<SpellIds> GetStartSpellIds(BreedIds breedId);
+    /// <param name="breedId">The ID of the breed to retrieve.</param>
+    /// <param name="breed">
+    /// When this method returns, contains the breed data if the breed ID exists; otherwise, null.
+    /// This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>
+    /// True if the breed data was successfully retrieved; otherwise, false.
+    /// </returns>
+    bool TryGetBreed(BreedIds breedId, [NotNullWhen(true)] out Breed? breed);
+
+    /// <summary>
+    /// Attempts to retrieve head data for the specified cosmetic ID.
+    /// </summary>
+    /// <param name="cosmeticId">The ID of the cosmetic to retrieve the head data for.</param>
+    /// <param name="head">
+    /// When this method returns, contains the head data if the cosmetic ID exists; otherwise, null.
+    /// This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>
+    /// True if the head data was successfully retrieved; otherwise, false.
+    /// </returns>
+    bool TryGetHead(int cosmeticId, [NotNullWhen(true)] out Head? head);
 }
