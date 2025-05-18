cbd1f6ff8f7f1752e06f32b55dc8f172646b67eb
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 22:56:30 +0200

    feat: add DeletedAt column to servers_characters table

--- src/Krosmoz.Servers.AuthServer/Database/Migrations/20250518205623_ServerCharactersDeletedAt.Designer.cs
+++ src/Krosmoz.Servers.AuthServer/Database/Migrations/20250518205623_ServerCharactersDeletedAt.Designer.cs
@@ -4,6 +4,7 @@ using System.Net;
 using Krosmoz.Servers.AuthServer.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Krosmoz.Servers.AuthServer.Database.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250518205623_ServerCharactersDeletedAt")]
+    partial class ServerCharactersDeletedAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Krosmoz.Servers.AuthServer/Database/Migrations/20250518205623_ServerCharactersDeletedAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Krosmoz.Servers.AuthServer.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ServerCharactersDeletedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "servers_characters",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "servers_characters");
+        }
+    }
+}
