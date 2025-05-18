e8499c9bc43c49316731f69f4936ee5c63b81100
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 02:28:32 +0200

    feat: add AccountId property to AccountCharacter model

--- src/Krosmoz.Servers.GameServer/Models/Accounts/AccountCharacter.cs
+++ src/Krosmoz.Servers.GameServer/Models/Accounts/AccountCharacter.cs
@@ -6,6 +6,8 @@ namespace Krosmoz.Servers.GameServer.Models.Accounts;
 
 public sealed class AccountCharacter
 {
+    public required int AccountId { get; init; }
+
     public required int ServerId { get; init; }
 
     public required long CharacterId { get; init; }
