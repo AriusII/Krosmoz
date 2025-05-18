64df9ced5a69fc6af76ab510559204e1c3a782f0
jimmy-mll <jimmy.mll@hotmail.com>
2025-05-18 18:34:13 +0200

    feat: add ExperienceRecord model for experience data tracking

---
+++ src/Krosmoz.Servers.GameServer/Database/Models/Experiences/ExperienceRecord.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Krosmoz 2025.
+// Krosmoz licenses this file to you under the MIT license.
+// See the license here https://github.com/AerafalGit/Krosmoz/blob/main/LICENSE.
+
+namespace Krosmoz.Servers.GameServer.Database.Models.Experiences;
+
+public sealed class ExperienceRecord
+{
+    public byte Level { get; set; }
+
+    public long CharacterXp { get; set; }
+
+    public long GuildXp { get; set; }
+
+    public long? JobXp { get; set; }
+
+    public long? MountXp { get; set; }
+
+    public long? AlignmentHonor { get; set; }
+
+    public ExperienceRecord(byte level, long characterXp, long guildXp, long? jobXp = null, long? mountXp = null, long? alignmentHonor = null)
+    {
+        Level = level;
+        CharacterXp = characterXp;
+        GuildXp = guildXp;
+        JobXp = jobXp;
+        MountXp = mountXp;
+        AlignmentHonor = alignmentHonor;
+    }
+}
